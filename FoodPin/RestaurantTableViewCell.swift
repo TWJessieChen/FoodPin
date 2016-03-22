a5d9a312509812abc72b4adbf7cb965eeadcc8d3
JessieChen <b09302083@gmail.com>
2016-03-22 22:45:21 +0800

    first commit

---
+++ FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by PO-CHUN CHEN on 2016/3/22.
+//  Copyright © 2016年 PO-CHUN CHEN. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
